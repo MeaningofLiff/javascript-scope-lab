7db0957563034c6594b41ead1f6dae0b94498155
Leah <leah.beder@gmail.com>
2025-08-06 17:57:08 -0500

    Flatburger Restaurant

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Step 1: Global Scope Variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Step 2: Function Scope
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// Step 3: Block Scope
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// Step 4: Change Global Variable
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
